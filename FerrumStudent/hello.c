92c77f5e25d9516c806866edb9f288704faf4540
ferrum-test-student <glebus99+ferrum@gmail.com>
2017-12-25 14:17:47 +0300

    break compile

--- FerrumStudent/hello.c
+++ FerrumStudent/hello.c
@@ -10,5 +10,8 @@ int main()
     printf("Hello world!\n");
     printf("2^3 = %f\n", cube(2));
     printf("DEBUG = %d\n", DEBUG);
+    
+    // This will generate an error (C++ style comments in C89)
+    
     return 0;
 }
